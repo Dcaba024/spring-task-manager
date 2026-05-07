6e0cc7310e4711d8ba1527ecfc7d1438e88e3cd4
Dylan Caballero <dylan@Dylans-MacBook-Pro.local>
2026-05-07 16:31:47 -0400

    backend clean build

--- backend/src/main/java/com/dylan/taskmanager/controller/TaskController.java
+++ backend/src/main/java/com/dylan/taskmanager/controller/TaskController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "${app.cors.allowed-origin}")
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
