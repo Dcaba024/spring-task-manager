42dd91c3a0c4487d760a85b936b1806b09074996
Dylan Caballero <dylan@Dylans-MacBook-Pro.local>
2026-05-08 17:58:10 -0400

    frontend call update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import './App.css'
 
-const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:8080/api/tasks";
+const API_URL = import.meta.env.PROD
+  ? "/api/tasks"
+  : import.meta.env.VITE_API_URL ?? "http://localhost:8080/api/tasks";
 
 function App() {
   const [tasks, setTasks] = useState([]);
